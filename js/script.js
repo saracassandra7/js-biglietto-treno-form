5b3531aafa245a1df66b07a5667ef15c13d22c41
saracassandra7 <saracassandra7@gmail.com>
2022-10-12 18:23:28 +0200

    [add] js for "dettaglio passeggeri"

--- js/script.js
+++ js/script.js
@@ -7,24 +7,14 @@ let NomeCognome;
 let kmPercorso;
 let etaUtente;
 
-const PrezzoBiglietto = kmPercorso * prezzoAlKm
+const primaCarrozza = 1
+const ultimaCarrozza = 7
+const minCodice = 10000
+const maxCodice = 50000
+
 let prezzoTot;
 let sconto;
 
-if (etaUtente < maggiorenne){
-  sconto = PrezzoBiglietto * scontoMaggiorenne;
-  prezzoTot = PrezzoBiglietto - sconto;
-  
-} else if (etaUtente >= over){
-  sconto = PrezzoBiglietto * scontoOver;
-  prezzoTot = PrezzoBiglietto - sconto;
-  
-} else {
-  prezzoTot = PrezzoBiglietto;
-
-}
-
-let prezzoFinale = prezzoTot.toFixed(2);
 
 //form del biglietto
 const btnGenera = document.getElementById('btn-genera');
@@ -33,6 +23,7 @@ btnGenera.addEventListener('click', function(){
   const NomeCognome = prompt('Qual è il tuo nome completo?');
   const kmPercorso = parseInt(prompt('Quanti km devi percorrere per il tuo viaggio?'));
   const etaUtente = parseInt(prompt('Quanti anni hai?'));
+  const PrezzoBiglietto = kmPercorso * prezzoAlKm
   
   
   document.getElementById('input-nome').value = NomeCognome;
@@ -40,13 +31,29 @@ btnGenera.addEventListener('click', function(){
 
   if (etaUtente < 18){
     document.getElementById('age').value = 'minorenne';
+    document.getElementById('offerta').innerHTML = 'Sconto del 20%'
+    sconto = PrezzoBiglietto * scontoMaggiorenne;
+    prezzoTot = PrezzoBiglietto - sconto;
 
   } else if (etaUtente >= 65){
     document.getElementById('age').value = 'over-65';
+    document.getElementById('offerta').innerHTML = 'Sconto del 40%'
+    sconto = PrezzoBiglietto * scontoOver;
+    prezzoTot = PrezzoBiglietto - sconto;
 
   }else{
     document.getElementById('age').value = 'maggiorenne';
+    document.getElementById('offerta').innerHTML = 'Biglietto Standard'
+    prezzoTot = PrezzoBiglietto;
   }
+
+  //dettagli biglietto
+
+  document.getElementById('nome-passeggero').innerHTML = NomeCognome;
+  document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * (ultimaCarrozza - primaCarrozza) + primaCarrozza);
+  document.getElementById('codice').innerHTML = Math.floor(Math.random() * (maxCodice - minCodice) + minCodice);
+  document.getElementById('costo-biglietto').innerHTML = `${prezzoTot.toFixed(2)} €`;
+  
 
 })
 
@@ -60,4 +67,3 @@ btnReset.addEventListener('click', function(){
   
 })
 
-//dettagli biglietto
